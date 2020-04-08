6dde752ac55aed109bc251f0bb1c304ca77f2a24
Pareekshith Katti <37375677+Paree24@users.noreply.github.com>
2020-04-08 17:58:15 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 import flask
+from flask_cors import CORS, cross_origin
 import pickle
 import numpy as np
 import pandas as pd
-app = flask.Flask(__name__)
 
+app = flask.Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/screen',methods = ['POST'])
+@cross_origin()
 def screen():
     req_data = flask.request.get_json()
     print(type(req_data))
+    print(req_data)
     test=pd.DataFrame(req_data)
     # df=pd.read_csv('notencoded.csv')
     # if df.columns[0]=='Unnamed: 0':
@@ -29,10 +34,8 @@ def screen():
     file=open('randomforest.sav','rb')
     model = pickle.load(file)
     level = model.predict(test)
-    info={
-        'level':int(level)
-    }
-    return info
+    print(int(level))
+    return str(level)
 
 
 app.run(host = "0.0.0.0", port = 55355)
