03207b8b2fb385d4010609049e330e555664c0cf
Pareekshith Katti <37375677+Paree24@users.noreply.github.com>
2020-04-10 15:59:07 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -3,6 +3,15 @@ from flask_cors import CORS, cross_origin
 import pickle
 import numpy as np
 import pandas as pd
+import sentry_sdk
+from sentry_sdk.integrations.flask import FlaskIntegration
+from sentry_sdk import capture_exception
+
+
+sentry_sdk.init(
+    dsn="https://89a7204bf6eb4860843afe8b5188ecaf@o372533.ingest.sentry.io/5196045",
+    integrations=[FlaskIntegration()]
+)
 
 app = flask.Flask(__name__)
 cors = CORS(app)
@@ -11,31 +20,26 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 @app.route('/screen',methods = ['POST'])
 @cross_origin()
 def screen():
-    req_data = flask.request.get_json()
-    print(type(req_data))
-    print(req_data)
-    test=pd.DataFrame(req_data)
-    # df=pd.read_csv('notencoded.csv')
-    # if df.columns[0]=='Unnamed: 0':
-    #     df=df.iloc[:,1:]
-    #Read Model from file
-    f=open('targetencodemodel.sav','rb')
-    t=pickle.load(f)
-    f.close()
-    print("Model Loaded")
-    print(t)
-    print('\n')
-    # cols=list(df.columns)
-    # cols.remove('Depression')
-    # print(cols)
-    test=t.transform(test)
-    print('done')
-    # df.to_csv('apienc.csv')
-    file=open('randomforest.sav','rb')
-    model = pickle.load(file)
-    level = model.predict(test)
-    print(int(level))
-    return str(level)
-
+    try:
+        req_data = flask.request.get_json()
+        print(type(req_data))
+        print(req_data)
+        test=pd.DataFrame(req_data)
+        f=open('targetencodemodel.sav','rb')
+        t=pickle.load(f)
+        f.close()
+        print("Model Loaded")
+        print(t)
+        print('\n')
+        test=t.transform(test)
+        print('done')
+        file=open('randomforest.sav','rb')
+        model = pickle.load(file)
+        level = model.predict(test)
+        print(int(level))
+        return str(level)
+    except Exception as e:
+        capture_exception(e)
+        flask.abort(400)
 
 app.run(host = "0.0.0.0", port = 55355)
